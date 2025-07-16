9dc1677d3752bec2b95fc1c5639da83d1ec2b240
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-07-16 20:53:13 +0300

    initial
    - theme is set

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return(
+    <div className='dmeo-container w-[100vw] h-[100vh] bg-solid-1 '>
+      <h1 className='bold-demo font-custom-bold font-bold text-6xl '>Whereas recognition of the inherent dignity</h1>
+      <h1 className='bold-demo font-custom-semibold text-6xl '>Whereas recognition of the inherent dignity</h1>
+      <h1 className='bold-demo font-custom-medium text-6xl '>Whereas recognition of the inherent dignity</h1>
+      <h1 className='bold-demo font-custom-regular text-6xl '>Whereas recognition of the inherent dignity</h1>
+      <h1 className='bold-demo font-custom-light text-6xl '>Whereas recognition of the inherent dignity</h1>
+    </div>
+  );
+}
+
+export default App;
