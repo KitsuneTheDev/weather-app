37abf37711606d3347dfcc76bc9de3daa5aa1375
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-07-16 21:06:21 +0300

    -update
    -fonts -is set as custom fonts

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return(
-    <div className='dmeo-container w-[100vw] h-[100vh] bg-solid-1 '>
+    <div className='demo-container w-[100vw] h-[100vh] bg-solid-1 '>
       <h1 className='bold-demo font-custom-bold font-bold text-6xl '>Whereas recognition of the inherent dignity</h1>
       <h1 className='bold-demo font-custom-semibold text-6xl '>Whereas recognition of the inherent dignity</h1>
       <h1 className='bold-demo font-custom-medium text-6xl '>Whereas recognition of the inherent dignity</h1>
